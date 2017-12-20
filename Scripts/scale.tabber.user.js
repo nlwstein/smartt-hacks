716641dc7d59a264907fb7335b0525c1c2162ad4
Nick Stein <nstein@gsngames.com>
2017-12-19 21:32:06 -0500

    Added first script to support tabbing between first & last name

---
+++ Scripts/scale.tabber.user.js
@@ -0,0 +1,22 @@
+﻿// ==UserScript==
+// @name          SCALE Tabber
+// @namespace     http://stein.boston
+// @description	  Provides tabbing functionality on student search page.
+// @include       *smartt*
+// ==/UserScript==
+
+(function ($) {
+    $(window).keydown(function (e) {
+        if (e.keyCode == 13 || e.which == 13) {
+            $('button[id="studentSearchForm:search"]').click();
+        }
+        if (e.keyCode == 9 || e.which == 9) {
+            if ($('input[id="studentSearchForm:lastName"]').is(":focus")) {
+                $('input[id="studentSearchForm:firstName"]').focus();
+            } else {
+                $('input[id="studentSearchForm:lastName"]').focus();
+            }
+            e.preventDefault();
+        }
+    });
+})(window.jQuery);
